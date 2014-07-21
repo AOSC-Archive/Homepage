bed357ef7d541b6a36a93fdf197d4850139ac8e8
LionNatsu <372691006@qq.com>
2014-07-21 23:33:15 +0800

    Basic css frame

--- script/jquery.onepage-scroll.js
+++ script/jquery.onepage-scroll.js
@@ -89,7 +89,7 @@
         topPos = 0,
         leftPos = 0,
         lastAnimation = 0,
-        quietPeriod = 500,
+        quietPeriod = 0,
         paginationList = "";
 
     $.fn.transformPage = function(settings, pos, index) {
